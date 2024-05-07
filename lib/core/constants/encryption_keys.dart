26d0650a0b6b9b37f7a56a1501ccb758f4adf567
mohamedmansour <mohamed.mansour.work@gmail.com>
2024-05-07 23:04:55 +0300

    set random encryption key

--- lib/core/constants/encryption_keys.dart
+++ lib/core/constants/encryption_keys.dart
@@ -1,4 +1,4 @@
 abstract class EncryptionKeys{
-  static const String authKey = "SJKDHBJ83H4CYGUOJJ9HBCGRX57CN0M";
-  static const String authIv = "CHW8NVFJM0HBH3BJBJ";
+  static const String authKey = "SJu2t3K0936DHBJ83H4CYd9296GUOJJ9HBCGRX57CN020M";
+  static const String authIv = "Cw9201jd8jw83HW8NVFJM0HBH3BJBJ";
 }
